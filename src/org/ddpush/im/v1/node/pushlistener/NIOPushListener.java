4c9cbf8a3e5efe58bea6e28ccbd92252a778ceb8
zdchuang <zdchuang@163.com>
2019-01-09 17:00:44 +0800

    b

--- src/org/ddpush/im/v1/node/pushlistener/NIOPushListener.java
+++ src/org/ddpush/im/v1/node/pushlistener/NIOPushListener.java
@@ -251,14 +251,10 @@ public class NIOPushListener implements Runnable {
 					// channel.socket().setSendBufferSize(1024);
 					PushTask task = new PushTask(this, channel);
 					SelectionKey key2 = channel.register(selector, SelectionKey.OP_READ, task);
-					//local
+					//local2
 					System.out.println(key2.interestOps() +" -- " + " -- "+ key2.channel() + " -- "+ key2.selector());
-//					System.out.println(channel.getLocalAddress() + " -- " + channel.getRemoteAddress());
-=======
-//					System.out.println(key2.interestOps() +" -- " + " -- "+ key2.channel() + " -- "+ key2.selector());
  					System.out.println(channel.getLocalAddress() + " -- " + channel.getRemoteAddress());
 					// test
->>>>>>> branch 'master' of https://github.com/ZDChuang/DDPush.git
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
